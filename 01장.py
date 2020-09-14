ca15ece9a9f0c1104c9d3c6fbdba2f37fb9726cf
sylee <remailme@hanmail.net>
2020-09-14 14:18:44 +0900

    알고리즘이란

---
+++ 01장.py
@@ -0,0 +1,23 @@
+#01-1
+
+#세정수의 최댓값 구하기
+
+print('세 정수의 최댓값을 구합니다.')
+a = int(input('정수 a의 값을 입력하세요.: '))
+b = int(input('정수 b의 값을 입력하세요.: '))
+c = int(input('정수 c의 값을 입력하세요.: '))
+
+maximum = a
+if b > maximum: maximum = b
+if c > maximum: maximum = c
+
+print('최댓값은 %d 입니다.' % maximum)
+print('최댓값은 {} 입니다..'.format(maximum))
+
+
+#print(f'최댓값은 {maximum}입니다.')
+#  File "<ipython-input-2-1e35c36865f5>", line 1
+#    print(f'최댓값은 {maximum}입니다.')
+#                              ^
+#SyntaxError: invalid syntax
+
