b38af4060c6058190fba04b6f15f129f1055952e
sylee <remailme@hanmail.net>
2020-09-14 14:46:33 +0900

    t2

--- 01장.py
+++ 01장.py
@@ -1,25 +1,47 @@
-#01-1
+#01장
+if 0 :
+    #01-1
+    
+    #세정수의 최댓값 구하기
+    #실습 1-1
+    
+    print('세 정수의 최댓값을 구합니다.')
+    a = int(input('정수 a의 값을 입력하세요.: '))
+    b = int(input('정수 b의 값을 입력하세요.: '))
+    c = int(input('정수 c의 값을 입력하세요.: '))
+    
+    maximum = a
+    if b > maximum: maximum = b
+    if c > maximum: maximum = c
+    
+    print('최댓값은 %d 입니다.' % maximum)
+    print('최댓값은 {} 입니다..'.format (maximum))
+    
+    #print(f'최댓값은 {maximum}입니다.')
+    #  File "<ipython-input-2-1e35c36865f5>", line 1
+    #    print(f'최댓값은 {maximum}입니다.')
+    #                              ^
+    #SyntaxError: invalid syntax
+    #C:\Users\hana1602a\Anaconda3>python --version
+    #Python 3.5.3 :: Anaconda 4.2.0 (32-bit)
+    # %s : 문자, %d : 정수, %f : 실수
+    
+    #실습1C-1
+    
+    print('이름을 입력하세요.: ', end='')
+    name = input()
+    print('안녕하세요? {} 님.'.format(name))
 
-#세정수의 최댓값 구하기
+#실습1-2
+#세 정수의 최댓값 구하기
 
-print('세 정수의 최댓값을 구합니다.')
-a = int(input('정수 a의 값을 입력하세요.: '))
-b = int(input('정수 b의 값을 입력하세요.: '))
-c = int(input('정수 c의 값을 입력하세요.: '))
-
-maximum = a
-if b > maximum: maximum = b
-if c > maximum: maximum = c
-
-print('최댓값은 %d 입니다.' % maximum)
-print('최댓값은 {} 입니다..'.format (maximum))
+def max3(a,b,c):
+    maximum = a
+    if b > maximum: maximum=b
+    if c > maximum: maximum = c
+    return maximum
+    
+print('max3(3,2,1) = {}'.format(max3(3,2,1)))
 
 
-#print(f'최댓값은 {maximum}입니다.')
-#  File "<ipython-input-2-1e35c36865f5>", line 1
-#    print(f'최댓값은 {maximum}입니다.')
-#                              ^
-#SyntaxError: invalid syntax
-#C:\Users\hana1602a\Anaconda3>python --version
-#Python 3.5.3 :: Anaconda 4.2.0 (32-bit)
-# %s : 문자, %d : 정수, %f : 실수
+
