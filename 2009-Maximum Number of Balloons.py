98a3cf3c3735560d9a485176e49c9d1265137a3d
sylee <remailme@hanmail.net>
2020-10-06 01:54:13 +0900

    1005-3

--- 2009-Maximum Number of Balloons.py
+++ 2009-Maximum Number of Balloons.py
@@ -148,3 +148,90 @@ print('elapse time: {:.9f} sec'.format(time.time() - stime))
 #""" Output:
 #('c', 2)
 #"""
+
+
+text = "krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw"
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        list1 = list(text)
+        ret = []
+        stop = 0
+        for _ in range(len(text)//7):
+            for s in 'balloon':
+                if s in list1:
+                    ret.append(list1.pop(list1.index(s)))
+#                    list1.remove(s)
+                else : 
+                    stop = 1
+                    break
+            if stop :
+                break     
+        return len(ret)//7
+
+'''Success
+Details 
+Runtime: 584 ms, faster than 5.65% of Python3 online submissions for Maximum Number of Balloons.
+Memory Usage: 14.3 MB, less than 5.24% of Python3 online submissions for Maximum Number of Balloons.'''
+    
+import time
+stime = time.time()
+sol = Solution()
+print(sol.maxNumberOfBalloons("nlaebolko"))
+print(sol.maxNumberOfBalloons("loonbalxballpoon"))
+print(sol.maxNumberOfBalloons(text))
+print('elapse time: {:.9f} sec'.format(time.time() - stime)) 
+
+#
+#text = "krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw"
+#
+#class Solution:
+#    def maxNumberOfBalloons(self, text: str) -> int:
+##        list1 = list(text)
+#        dic1 = {k:v for k,v in enumerate(text)}
+#        dic2 = {v:k for k,v in enumerate(text)}
+#        ret = []
+#        stop = 0
+#        for _ in range(len(text)//7):
+#            for s in 'balloon':
+#                if s in dic2:
+##                    ret.append(list1.pop(list1.index(s)))
+#                    ret.append(dic1[dic2.pop(s)])
+##                    list1.remove(s)
+#                else : 
+#                    stop = 1
+#                    break
+#            if stop :
+#                break     
+#        return len(ret)//7
+#
+#'''Success
+#Details 
+#Runtime: 584 ms, faster than 5.65% of Python3 online submissions for Maximum Number of Balloons.
+#Memory Usage: 14.3 MB, less than 5.24% of Python3 online submissions for Maximum Number of Balloons.'''
+#    
+#import time
+#stime = time.time()
+#sol = Solution()
+#print(sol.maxNumberOfBalloons("nlaebolko"))
+#print(sol.maxNumberOfBalloons("loonbalxballpoon"))
+#print(sol.maxNumberOfBalloons(text))
+#print('elapse time: {:.9f} sec'.format(time.time() - stime))
+#
+#text = "loonbalxballpoon"
+#dic1 = {k:v for k,v in enumerate(text)}
+##dic2 = {v:k for k,v in enumerate(text)}
+#list2 = list(text)
+#ret = []
+#stop = 0
+#for _ in range(len(text)//7):
+#    for s in 'balloon':
+#        if s in dic1.values():
+##            print(s)
+#            ret.append(list2.pop([dic1(s)]))
+#        else : 
+#            stop = 1
+#            break
+#    if stop :
+#        break     
+#print(ret)
